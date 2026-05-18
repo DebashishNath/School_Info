5a7df38813d625e108c7770cc84f0a3e4f705c36
Debashish <debasis.nath@gmail.com>
2026-05-18 12:23:36 +0530

    checked in source codes of school
    checked in source codes of school

---
+++ school_source_codes/src/test/java/school_source_codes/SchoolSourceCodesApplicationTests.java
@@ -0,0 +1,13 @@
+package school_source_codes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolSourceCodesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
